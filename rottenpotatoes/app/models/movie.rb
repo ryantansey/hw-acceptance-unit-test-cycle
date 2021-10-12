bb3d3d13410ed7801892fb7f7540b7513ff38e6b
ryantansey <rtansey96@gmail.com>
2021-10-12 01:33:50 +0000

    complete part 2

--- rottenpotatoes/app/models/movie.rb
+++ rottenpotatoes/app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ActiveRecord::Base
+    def self.find_same_director director
+        Movie.where(:director => director)
+    end
 end
--- rottenpotatoes/config/routes.rb
+++ rottenpotatoes/config/routes.rb
@@ -2,4 +2,5 @@ Rottenpotatoes::Application.routes.draw do
   resources :movies
   # map '/' to be a redirect to '/movies'
   root :to => redirect('/movies')
+  match 'movies/:id/directors', to: 'movies#directors', as: 'search_directors', via: [:get]
 end
